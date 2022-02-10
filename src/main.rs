115161994ce6908f84757271884f5e57bdd45c3d
Miles <miles@karmacheck.com>
2022-02-10 17:23:05 -0600

    test

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,49 @@ static GREEN: &'static str = "green";
 static YELLOW: &'static str = "yellow";
 static GRAY: &'static str = "gray";
 
+
+struct PermutationScore {
+    probability: i32,
+    information: i32,
+}
+
 fn main() {
     let solutions = vec!["joker".to_string(), "poker".to_string(), "loner".to_string()];
 
     for solution in solutions.clone() {
-        let permutations = generate_color_permutations();
+        let mut permutations = generate_color_permutations();
         for guess in solutions.clone() {
             let feedback = get_feedback(solution.clone(), guess.clone());
-            println!("{}-{}", solution, guess);
-            let trimmed = trim_possibles(solutions.clone(), feedback, guess.clone());
+            let trimmed = trim_possibles(solutions.clone(), feedback.clone(), guess.clone());
             let eliminated_count = (solutions.len() - trimmed.len()) as i32;
-            let mut map: HashMap<String, i32> = HashMap::new();
-            map.insert("p".to_string(), permutations[&feedback]["p"] + 1);
-            map.insert("i".to_string(), permutations[&feedback]["i"] + eliminated_count);
-            permutations[&feedback] = permutations[&feedback];
-            println!("{}", eliminated_count);
+            permutations.insert(feedback.clone(), PermutationScore{
+                information: permutations.get_key_value(&feedback.clone()).unwrap().1.information + eliminated_count,
+                probability: permutations.get_key_value(&feedback.clone()).unwrap().1.probability + 1
+            });
+        }
+
+        for p in permutations {
+            println!("{:?}", p.1.probability);
         }
     }
+
 
 }
 
 //how probably is sequence (green, yellow, gray) * bits of information log2(1/p)
 
 //gets every combo feedback could be returned
-fn generate_color_permutations() -> HashMap<Vec<String>, HashMap<String, i32>>{
+fn generate_color_permutations() -> HashMap<Vec<String>, PermutationScore>{
     let combinations = [GREEN.to_string(), YELLOW.to_string(), GRAY.to_string()].into_iter().combinations_with_replacement(5).collect_vec();
-    let mut permutations: HashMap<Vec<String>, HashMap<String, i32>> = HashMap::new();
+    let mut permutations: HashMap<Vec<String>, PermutationScore> = HashMap::new();
     for combination in combinations{
         let ps = combination.into_iter().permutations(5).unique().collect_vec();
         for permutation in ps {
-            let mut map: HashMap<String, i32> = HashMap::new();
-            map.insert("p".to_string(), 0);
-            map.insert("i".to_string(), 0);
-            permutations.insert(permutation, map);
+                let perm = PermutationScore{
+                    information: 0,
+                    probability: 0
+                };
+            permutations.insert(permutation, perm);
         }
 
     }
