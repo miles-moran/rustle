0fd64e92dbf36c35cf12ff1c6af0a0bbaa659bc7
Miles <miles@karmacheck.com>
2022-02-10 13:55:27 -0600

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,21 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 static GREEN: &'static str = "green";
 static YELLOW: &'static str = "yellow";
 static GRAY: &'static str = "gray";
 
 fn main() {
-    let solutions = vec!["joker", "poker", "loner"];
+    let solutions = vec!["joker".to_string(), "poker".to_string(), "loner".to_string()];
     let permutations = generate_color_permutations();
 
     for solution in solutions.clone() {
-        for possible in solutions.clone().iter() {
-            let mut possibles = vec![];
-            for permutation in permutations.iter() {
-                
-            
-                let mut add = true;
-                for i in 0..5 {
-                    let solution_char = solution.chars().nth(i).unwrap();
-                    let possible_char = possible.chars().nth(i).unwrap();
-                    let color = permutation.iter().nth(i).unwrap();
-                    if color == GREEN {
-                        if solution_char != possible_char {
-                            add = false
-                        }
-                    }
-                    if color == YELLOW {
-                        if solution_char == possible_char {
-                            add = false
-                        }
-                        if !possible.contains(solution_char) {
-                            add = false
-                        }
-                    }
-                    if color == GRAY {
-                        if possible.contains(solution_char) {
-                            add = false
-                        }
-                    }
-                }
-                if add == true {
-                    possibles.push(possible.clone());
-                }
-            }
-            let eliminated = solutions.len() - possibles.len();
-            if eliminated != 3 {
-                println!("{}", eliminated);
-            }
+        for guess in solutions.clone() {
+            let feedback = get_feedback(solution.clone(), guess.clone());
+            println!("{}-{}", solution, guess);
+            let trimmed = trim_possibles(solutions.clone(), feedback, guess.clone());
+            let eliminated_count = solutions.len() - trimmed.len();
+            println!("{}", eliminated_count);
         }
     }
 }
@@ -60,4 +30,65 @@ fn generate_color_permutations() -> Vec<Vec<String>>{
        permutations.append(&mut combination.into_iter().permutations(5).unique().collect_vec());
     }
     return permutations;
+}
+
+fn get_feedback(solution:String, possible:String) -> Vec<String> {
+    let mut feedback: Vec<String> = Vec::new();
+    for i in 0..5 {
+        let solution_char = solution.chars().nth(i).unwrap();
+        let possible_char = possible.chars().nth(i).unwrap();
+
+        if solution_char == possible_char{
+            feedback.push(GREEN.to_string());
+        }
+
+        if solution_char != possible_char{
+            if solution.contains(possible_char){ //TODO: Gray occurence if occurenes in possible > occurences in solution gray
+                feedback.push(YELLOW.to_string())
+            }
+
+            if !solution.contains(possible_char){
+                feedback.push(GRAY.to_string())
+            }
+        }
+    }
+    return feedback
+}
+
+fn trim_possibles(possibles: Vec<String>, feedback: Vec<String>, guess: String) -> Vec<String> {
+    let mut trimmed: Vec<String> = Vec::new();
+    for possible in possibles.clone() {
+        let mut add = true;
+        for i in 0..5 {
+            let color = feedback.iter().nth(i).unwrap();
+            let guess_char = guess.chars().nth(i).unwrap();
+            let possible_char = possible.chars().nth(i).unwrap();
+           
+            if color == GREEN {
+                if guess_char != possible_char{
+                    add = false;
+                }
+            }
+
+            if color == YELLOW {
+                if guess_char == possible_char{
+                    add = false;
+                }
+                if !possible.contains(guess_char) {
+                    add = false;
+                }
+            }
+
+            if color == GRAY {
+                if possible.contains(guess_char) { //TODO: gray occurences
+                    add = false;
+                }
+            }
+            
+        }
+        if add == true {
+            trimmed.push(possible);
+        }
+    }
+    return trimmed
 }
